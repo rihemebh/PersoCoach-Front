fae7c75ef5e21eb4042bc634f8c571c64c68d942
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2021-06-13 15:00:51 +0100

    prog

---
+++ .history/src/Coaches/Components/detailsModal_20210612223044.js
@@ -0,0 +1,251 @@
+import React from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import {
+  Modal,
+  Row,
+  Col,
+  Container,
+  Button,
+  Form,
+  Input,
+  FormGroup,
+  Label,
+  FormText,
+} from "reactstrap";
+import axios from "axios";
+import { toast, ToastContainer,Zoom , Bounce } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+export default class CoachModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.id,
+      response: "",
+      modal: false,
+      name: "",
+      gender: "",
+      goal: "",
+      age: 0,
+      height: 0.0,
+      weight: 0.0,
+      pic: null,
+      practice: "",
+      disabled : this.props.disabled,
+      user: JSON.parse(localStorage.getItem("user")), 
+    };
+  
+   
+  }
+
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+
+
+
+  closeModal() {
+    this.setState({ name: false, error: false });
+  }
+
+  async getuser(){
+    const currentUser =  JSON.parse(localStorage.getItem("user"));
+ 
+  //  const API_URL_AUTH = "http://localhost:8080/api/auth/";
+const API_URL_USER = "http://localhost:8080/api/user/";
+    const response =  await axios.get(API_URL_USER + "id/" + currentUser.id, {
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.accessToken
+         }
+        });
+    return response.data;
+ 
+   
+  }
+   render() {
+    
+    const currentUser =  JSON.parse(localStorage.getItem("user"));
+
+    return (
+      <Container>
+      
+        <Row id="modals">
+          <Col>
+          
+         :  <Button
+              className="btn-round"
+              color="link"
+              outline
+              type="button"
+              onClick={this.toggleModal}
+        
+            >
+                <div>
+    
+        <ToastContainer 
+        draggable={false} 
+        transition={Zoom}
+        autoClose={4000}
+        position="bottom-right"
+        hideProgressBar={false}
+        />
+      </div>
+             View Program details
+            </Button>
+           
+            {/* Modal */}
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+              <div className="modal-header">
+                <button
+                  aria-label="Close"
+                  className="close"
+                  type="button"
+                  onClick={this.toggleModal}
+                >
+                  <span aria-hidden={true}></span>
+                </button>
+                <h5 className="modal-title text-center" id="exampleModalLabel">
+                  Brief program
+                </h5>
+              </div>
+              <div className="modal-body"
+               style={{ maxHeight: "calc(100vh - 210px)", overflowY: "auto" }}>
+                <h6> Welcome to the journey</h6>
+                Once you fill this form you will get a brief personalized
+                program from the coach in 2 or 3 days
+                <br></br><br></br>
+               
+                <br></br>
+              
+           
+              <div className="container">
+                <Container>
+                  <AvForm
+                    onValidSubmit={this.handleValidSubmit}
+                    onInvalidSubmit={this.handleInvalidSubmit}
+                  >
+                    <h6>
+                    Name <span className='text-danger'>*</span>
+                      <AvField name="name" type="text" required />
+                    </h6>
+                    <h6>
+                    Age <span className='text-danger'>*</span></h6>
+                      <AvField name="age" type="number" required />
+                    
+                    <FormGroup>
+                      <Label for="exampleSelect">
+                        <h6>Gender <span className="text-danger">*</span></h6>
+                      </Label>
+                      <AvField
+                        type="select"
+                        name="select"
+                        id="exampleSelect"
+                        required
+                      >
+                        <option>-- Not selected --</option>
+                        <option>Male</option>
+                        <option>Female</option>
+                      </AvField>
+                    </FormGroup>
+                    <h6>
+                    Weight <span className='text-danger'>*</span>
+                      <AvField
+                        name="weight"
+                     
+                        type="number"
+                        required
+                      />
+                    </h6>
+                    <h6>
+                    Height <span className='text-danger'>*</span>
+                      <AvField
+                        name="height"
+                      
+                        type="number"
+                        required
+                      />
+                    </h6>
+                    <FormGroup>
+                      <Label for="exampleSelect1">
+                        <h6>How often do you exercice ? <span className="text-danger">*</span></h6>
+                      </Label>
+                      <AvField
+                        type="select"
+                        name="select1"
+                        id="exampleSelect1"
+                        required
+                      >
+                        <option>-- Not selected --</option>
+                        <option>Always</option>
+                        <option>Sometimes</option>
+                        <option>Rarely</option>
+                        <option>Never</option>
+                      </AvField>
+                    </FormGroup>
+                    <FormGroup>
+                      <h6>
+                      Goal <span className='text-danger'>*</span>
+                        </h6>
+                        <AvField
+                          name="goal"
+                        
+                          type="textarea"
+                          required
+                        ></AvField>
+                      <FormText>
+                        Example: I want to change my lifestyle / I want to loose
+                        weight ...
+                      </FormText>
+                    </FormGroup>
+                    <br></br>
+
+              {/*      <h6>
+                      {" "}
+                      <AvField
+                        type="file"
+                        name="file"
+                        label="Upload a full picture of you"
+                        id="exampleFile"
+                      />
+                    </h6>
+
+                    {/* <AvField type="radio"  name="radio1" required></AvField> <span> I accept all the terms and conditions *</span>
+                     */}
+                          <div className="modal-footer">
+                      <div className="">
+                        <Button type="submit" onValidSubmit={this.handleValidSubmit} >
+                          Submit your request
+                        </Button>
+                      </div>
+                      <div className="">
+                        <Button
+                          className="btn-link"
+                          color="default"
+                          type="button"
+                          onClick={this.toggleModal}
+                        >
+                          Back
+                        </Button>
+                      </div>
+                    </div>
+                         </AvForm>
+                         </Container></div>
+                
+             
+               
+                  
+           
+                
+              </div>
+            </Modal>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
---
+++ .history/src/Coaches/Components/detailsModal_20210612224446.js
@@ -0,0 +1,171 @@
+import React from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import {
+  Modal,
+  Row,
+  Col,
+  Container,
+  Button,
+  Table,
+  Form,
+  Input,
+  FormGroup,
+  Label,
+  FormText,
+} from "reactstrap";
+import axios from "axios";
+import { toast, ToastContainer,Zoom , Bounce } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+export default class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     request: this.props.request,
+      program: {}
+    };
+  
+   
+  }
+  componentDidMount(){
+console.log(this.state.request)
+    axios.get("http://localhost:8080/api/bprogram/request",this.state.request).then(function(response){
+        this.setState({program : response.data});
+      console.log(response.data);
+    }.bind(this)).catch((error)=> console.log(error));
+
+  }
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+
+
+
+  closeModal() {
+    this.setState({ name: false, error: false });
+  }
+
+  async getuser(){
+    const currentUser =  JSON.parse(localStorage.getItem("user"));
+ 
+  //  const API_URL_AUTH = "http://localhost:8080/api/auth/";
+const API_URL_USER = "http://localhost:8080/api/user/";
+    const response =  await axios.get(API_URL_USER + "id/" + currentUser.id, {
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.accessToken
+         }
+        });
+    return response.data;
+ 
+   
+  }
+   render() {
+       const program = this.state.program;
+    console.log(program);
+
+    //const currentUser =  JSON.parse(localStorage.getItem("user"));
+
+    return (
+      <Container>
+      
+        <Row id="modals">
+          <Col>
+          
+          <Button
+              className="btn-round"
+              color="link"
+             
+            
+              onClick={this.toggleModal}
+        
+            >
+                <div>
+    
+     
+      </div>
+             View Program details
+            </Button>
+           
+            {/* Modal */}
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+              <div className="modal-header">
+                <button
+                  aria-label="Close"
+                  className="close"
+                  type="button"
+                  onClick={this.toggleModal}
+                >
+                  <span aria-hidden={true}></span>
+                </button>
+                <h5 className="modal-title text-center" id="exampleModalLabel">
+                  Brief program
+                </h5>
+              </div>
+              <div className="modal-body"
+               style={{ maxHeight: "calc(100vh - 210px)", overflowY: "auto" }}>
+              
+              <Table>
+  <thead>
+  <tr>
+          
+          <th>Program Id</th>
+          <th>Duration </th>
+          <th>Frequency</th>
+          <th>Diet Type</th>
+          <th>Description</th>
+          
+          
+        </tr>
+
+
+  </thead>
+  <tbody>
+<tr>
+<td>{program.id} </td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+
+</tr>
+  </tbody>
+</Table>
+             
+            
+           
+           
+                          <div className="modal-footer">
+                 
+                      <div className="">
+                        <Button
+                          className="btn-link"
+                          color="default"
+                          type="button"
+                          onClick={this.toggleModal}
+                        >
+                          Back
+                        </Button>
+                      </div>
+                    </div>
+                        
+                       
+                         </div>
+                
+             
+               
+                  
+           
+         
+            </Modal>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
