19be238852f497380e45e04111a971a1ed96a2a5
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2021-06-14 09:45:36 +0100

    fix program details + fix password update

---
+++ .history/src/views/accueil/AccueilHeader_20210613173809.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function AccueilHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/twoTraining.jpg").default + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">PersoCoach</h1>
+              <div className="fog-low">
+             
+              </div>
+            
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              All the personal coaches in just one place and it's up to you to choose!
+            </h2>
+          </Container>
+        </div>
+        </div>
+    </>
+  );
+}
+
+export default AccueilHeader;
---
+++ .history/src/views/accueil/What_20210613174457.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import CarouselCoach from "views/accueil/CarouselCoach.js";
+import {
+    Container,
+    Row,
+    Col,
+  } from "reactstrap";
+  
+const What = () =>{
+    return(
+        <>
+        <Container style={{height: "350px"}}>
+        <Row >
+        
+        <Col xs="6" >
+       
+        <div className="typography-line" style={{marginTop: "70px"}}>
+            <h4>What is PersoCoach? </h4>
+                <p className="mb-0" >
+                PersoCoach is where coaches and fitness seekers meet and schedule their workout programs.<br></br>
+                Tryout tips, success stories,or the best recipes- PersoCoach has you covered up.
+                </p>
+                <br />
+              
+            </div>
+        </Col>
+        <Col xs="2"></Col>
+        <Col xs="4">
+   
+        <img
+                  alt="..."
+                  
+                  src={require("assets/img/whatTransparentresized.png").default}
+                />
+            
+        </Col>
+        </Row>
+        </Container>
+        </>
+    )
+}
+export default What;
