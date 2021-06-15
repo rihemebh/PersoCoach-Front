1a71bd187dde089f573a4b2318084c6452afdf05
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2021-06-15 19:06:09 +0100

    review

---
+++ .history/src/Clients/ReviewModal_20210615133755.js
@@ -0,0 +1,177 @@
+import React from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import {
+  Modal,
+  Row,
+  Col,
+  Container,
+  Button,
+  Form,
+  Input,
+  FormGroup,
+  Label,
+  FormText,
+} from "reactstrap";
+import axios from "axios";
+import { toast, ToastContainer,Zoom , Bounce } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import authHeader from "Authentification/AuthHeader";
+import FontAwesome from "react-fontawesome";
+
+
+export default class ReviewModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+      
+    };
+   
+    this.showtoastSuccess=this.showtoastSuccess.bind(this);
+    this.handleValidSubmit = this.handleValidSubmit.bind(this);
+    this.handleInvalidSubmit = this.handleInvalidSubmit.bind(this);
+  
+
+  }
+
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+   showtoastSuccess = ()=>{
+     return toast("Your Review was successfully sent");
+   }
+   
+   showtoastFail = ()=>{
+    return toast("We can't add you review");
+   }
+  
+  /*async componentDidMount(){
+    this.setState({
+      userInfo: this.props.currentUserInfo
+    })
+  }*/
+
+
+
+  handleInvalidSubmit(event, errors, values) {
+    this.setState({ name: values.name, error: true });
+  }
+
+  closeModal() {
+    this.setState({ name: false, error: false });
+  }
+
+  async handleValidSubmit(e, values) {
+
+
+
+    this.toggleModal();
+      
+   
+  };
+  
+  render() {
+   
+    console.log(this.state)
+
+    //console.log(this.state.userInfo)
+    return (
+      <Container>
+      
+        <Row id="modals">
+          <Col>
+            {/* Button trigger modal */}
+            <Button
+              className="btn-round"
+              color="danger"
+              outline
+              type="button"
+              onClick={this.toggleModal}
+              
+            >   <FontAwesome className="fa-star" ></FontAwesome>Rate This Coach     
+                <div>
+    
+        <ToastContainer 
+        draggable={false} 
+        transition={Zoom}
+        autoClose={4000}
+        position="bottom-right"
+        hideProgressBar={false}
+        />
+      </div>
+            
+            </Button>
+            {/* Modal */}
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+              <div className="modal-header">
+                <button
+                  aria-label="Close"
+                  className="close"
+                  type="button"
+                  onClick={this.toggleModal}
+                >
+                  <span aria-hidden={true}></span>
+                </button>
+                <h5 className="modal-title text-center" id="exampleModalLabel">
+               Give a Review for <span style={{fontWeight: "bold"}}>{this.props.coach.name}  </span>         
+               
+                </h5>
+              </div>
+              <div className="modal-body"
+               style={{ maxHeight: "calc(100vh - 210px)", overflowY: "auto" }}>
+              
+                <div class="container">
+                <Container>
+                
+                  <AvForm onValidSubmit={this.handleValidSubmit}>
+                    <h6>
+                      <AvField 
+                          name="text" 
+                          label=" Review text" 
+                          type="text" 
+                         
+                          
+                    />
+                    </h6>
+              
+            
+
+
+              
+             
+                    <div className="modal-footer">
+                      <div className="">
+                      <FormGroup>
+                        <Button type="submit" >
+                          Send you review
+                        </Button>
+                        </FormGroup>
+                      </div>
+                      <div className="">
+                        <Button
+                          className="btn-link"
+                          color="default"
+                          type="button"
+                          onClick={this.toggleModal}
+                        >
+                          Back
+                        </Button>
+                      </div>
+                    </div>
+                    </AvForm>
+                  </Container>
+                </div>
+                  
+           
+                
+              </div>
+            </Modal>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
---
+++ .history/src/Clients/ReviewModal_20210615134540.js
@@ -0,0 +1,185 @@
+import React from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import {
+  Modal,
+  Row,
+  Col,
+  Container,
+  Button,
+  Form,
+  Input,
+  FormGroup,
+  Label,
+  FormText,
+} from "reactstrap";
+import axios from "axios";
+import { toast, ToastContainer,Zoom , Bounce } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import authHeader from "Authentification/AuthHeader";
+import FontAwesome from "react-fontawesome";
+
+
+export default class ReviewModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        checkedRate:[false,false,false,false,false],
+     
+      
+    };
+   
+    this.showtoastSuccess=this.showtoastSuccess.bind(this);
+    this.handleValidSubmit = this.handleValidSubmit.bind(this);
+    this.handleInvalidSubmit = this.handleInvalidSubmit.bind(this);
+  
+
+  }
+
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+   showtoastSuccess = ()=>{
+     return toast("Your Review was successfully sent");
+   }
+   
+   showtoastFail = ()=>{
+    return toast("We can't add you review");
+   }
+  
+  /*async componentDidMount(){
+    this.setState({
+      userInfo: this.props.currentUserInfo
+    })
+  }*/
+
+
+
+  handleInvalidSubmit(event, errors, values) {
+    this.setState({ name: values.name, error: true });
+  }
+
+  closeModal() {
+    this.setState({ name: false, error: false });
+  }
+
+  async handleValidSubmit(e, values) {
+
+
+
+    this.toggleModal();
+      
+   
+  };
+  
+  render() {
+    let x = this.state.checkedRate;
+   
+    {x.map((value, index) => {
+        return <IconCheckbox checked={this.state.checkedRate[index]} 
+        checkedIcon={ <FontAwesome className="fa-star text-warning" name="star" style={style}  />} 
+        uncheckedIcon={ <FontAwesome className="fa-star" name="star" style={style} />}
+        onClick={()=> this._onCheckboxClicked(index)} 
+        />
+    })
+  }
+    return (
+      <Container>
+      
+        <Row id="modals">
+          <Col>
+         
+            <Button
+              className="btn-round"
+              color="info"
+              outline
+              type="button"
+              onClick={this.toggleModal}
+              
+            >  Rate This Coach     
+                <div>
+    
+        <ToastContainer 
+        draggable={false} 
+        transition={Zoom}
+        autoClose={4000}
+        position="bottom-right"
+        hideProgressBar={false}
+        />
+      </div>
+            
+            </Button>
+            {/* Modal */}
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+              <div className="modal-header">
+                <button
+                  aria-label="Close"
+                  className="close"
+                  type="button"
+                  onClick={this.toggleModal}
+                >
+                  <span aria-hidden={true}></span>
+                </button>
+                <h5 className="modal-title text-center" id="exampleModalLabel">
+               Give a Review for <span style={{fontWeight: "bold"}}>{this.props.coach.name}  </span>         
+               
+                </h5>
+              </div>
+              <div className="modal-body"
+               style={{ maxHeight: "calc(100vh - 210px)", overflowY: "auto" }}>
+              
+                <div class="container">
+                <Container>
+                
+                  <AvForm onValidSubmit={this.handleValidSubmit}>
+                    <h6>
+                    <FontAwesome className="fa-comments" ></FontAwesome> Review text:
+                      <AvField 
+                          name="text" 
+                     
+                          type="text" 
+                         
+                          
+                    />
+                    </h6>
+              
+            
+
+
+              
+             
+                    <div className="modal-footer">
+                      <div className="">
+                      <FormGroup>
+                        <Button type="submit" >
+                          Send you review
+                        </Button>
+                        </FormGroup>
+                      </div>
+                      <div className="">
+                        <Button
+                          className="btn-link"
+                          color="default"
+                          type="button"
+                          onClick={this.toggleModal}
+                        >
+                          Back
+                        </Button>
+                      </div>
+                    </div>
+                    </AvForm>
+                  </Container>
+                </div>
+                  
+           
+                
+              </div>
+            </Modal>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
